f3e71a2da11e7e29e89592bd20b2f4e51d993441
Son <guswns1659@gmail.com>
2019-12-22 22:52:43 +0900

    Practice 3 kinds of classes

--- TIL/index/Book.java
+++ TIL/index/Book.java
@@ -4,22 +4,27 @@ package index;
 public class Book {
     private String title;
     private int price;
-    public Book() {}
-    public Book(String title, int price){
+    public Book() {   }
+
+    public Book(String title, int price) {
         this.title = title;
         this.price = price;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
+
     public String getTitle() {
         return title;
     }
-    public void setPrice(int price) {
-        this.price = price;
+
+    public void setTitle(String title) {
+        this.title = title;
     }
+
     public int getPrice() {
         return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 
     @Override
@@ -28,5 +33,13 @@ public class Book {
                 "title='" + title + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    public int hap() {
+        int sum = 0;
+        for (int i = 1; i < 11; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
--- TIL/index/index.java
+++ TIL/index/index.java
@@ -1,9 +1,22 @@
 package index;
+import com.google.gson.Gson;
 
 public class index {
-    // Review Setter & Getter Method
     public static void main(String[] args) {
-        Book book = new Book("java", 10000);
-        System.out.println(book);
+        // 1. 자바가 기본으로 제공하는 클래스..
+        String str;
+        str = "I love you";
+        int index = str.indexOf("I");
+        System.out.println(index);
+
+        // 2. 내가 직접 설계하는 클래스.. DTO, DAO, Utility
+        Book book = new Book("java", 15000);
+        int answer = book.hap();
+        System.out.println(answer);
+
+        // 3. 외부에서 가져오는 클래스 .. Gson
+        Gson g = new Gson();
+        String json = g.toJson(book);
+        System.out.println(json);
     }
 }
