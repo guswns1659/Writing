f6dc8caea4dae450c16976e695c38c7d84e9c453
Son <guswns1659@gmail.com>
2019-12-23 12:56:05 +0900

    Studying Inheritance and Override

---
+++ TIL/index/Animal.java
@@ -0,0 +1,7 @@
+package index;
+
+public class Animal {
+    public void eat(){
+        System.out.println("와구와구");
+    }
+}
--- TIL/index/index.java
+++ TIL/index/index.java
@@ -3,20 +3,10 @@ import com.google.gson.Gson;
 
 public class index {
     public static void main(String[] args) {
-        // 1. 자바가 기본으로 제공하는 클래스..
-        String str;
-        str = "I love you";
-        int index = str.indexOf("I");
-        System.out.println(index);
+        Dog d = new Dog();
+        d.eat();
 
-        // 2. 내가 직접 설계하는 클래스.. DTO, DAO, Utility
-        Book book = new Book("java", 15000);
-        int answer = book.hap();
-        System.out.println(answer);
-
-        // 3. 외부에서 가져오는 클래스 .. Gson
-        Gson g = new Gson();
-        String json = g.toJson(book);
-        System.out.println(json);
+        Cat c = new Cat();
+        c.eat();
     }
 }
