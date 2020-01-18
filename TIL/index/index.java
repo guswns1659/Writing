b917d3a6a1b31a9431025ccaf5a59f3575bea6ba
HYUNJUN <guswns1659@gmail.com>
2020-01-18 21:34:17 +0900

    20.01.18) Implement of development environmnet in ubuntu

--- TIL/index/index.java
+++ TIL/index/index.java
@@ -6,22 +6,6 @@ import java.util.Scanner;
 public class index {
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        ArrayList<String> inputs = new ArrayList<>();
-
-        System.out.println("숫자를 입력해주세요");
-
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            if (input == null || input.isEmpty()) {
-                break;
-            }
-            inputs.add(input);
-        }
-        sc.close();
-
-        for (int i = 0; i < inputs.size(); ++i) {
-            System.out.println(inputs.get(i));
-        }
+        System.out.println("hello world");
     }
 }
