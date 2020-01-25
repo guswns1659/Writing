4aeb494e205825f1d679ef8d89ef5b7ab7019efb
HYUNJUN <guswns1659@gmail.com>
2020-01-25 21:03:55 +0900

    20.01.25) Update 2020-01-25-Enum.md

--- Code/JavaStudying/EnumMain.java
+++ Code/JavaStudying/EnumMain.java
@@ -1,6 +1,32 @@
 package JavaStudying;
 
+enum Fruit {
+    APPLE("red"), PEACH("pink"), BANANA("yellow");
+
+    private final String color;
+
+    Fruit(String color) {
+        System.out.println("call constructor "+this);
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+}
+
 public class EnumMain {
     public static void main(String[] args) {
+        Fruit type = Fruit.APPLE;
+        switch (type) {
+            case APPLE:
+                System.out.println(57+"kcal, color : "+Fruit.APPLE.getColor());
+                break;
+            case PEACH:
+                System.out.println(34+"kcal, color : "+Fruit.PEACH.getColor());
+                break;
+            case BANANA:
+                System.out.println(93+"kcal, color : "+Fruit.BANANA.getColor());
+                break;
+        }
     }
 }
