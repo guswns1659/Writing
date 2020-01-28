6193f6222d20525171ab4febe893ae575adafd6d
HYUNJUN <guswns1659@gmail.com>
2020-01-28 11:25:43 +0900

    20.01.28) Create Lambda1.java

---
+++ Code/JavaStudying/Lambda1.java
@@ -0,0 +1,26 @@
+package JavaStudying;
+
+interface Printable {
+    void print(String s);
+}
+
+//class Printer implements Printable{
+//
+//    @Override
+//    public void print(String s) {
+//        System.out.println(s);
+//    }
+//}
+
+public class Lambda1 {
+    public static void showString(Printable p, String s){
+        p.print(s);
+    }
+    public static void main(String[] args) {
+        showString(s -> {
+            System.out.println(s);
+        }, "What is Lamdba?");
+    }
+}
+
+
