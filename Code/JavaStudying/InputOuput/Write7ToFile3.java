c9493a7bc2f2087cad7ce724e0bce81cef1113b0
guswns1659 <guswns1659@gmail.com>
2020-02-02 14:45:35 +0900

    20.02.02) Update 2020-02-02-Java - Input&Output.md

--- Code/JavaStudying/InputOuput/Write7ToFile3.java
+++ Code/JavaStudying/InputOuput/Write7ToFile3.java
@@ -8,20 +8,19 @@ public class Write7ToFile3 {
         input();
     }
 
-    private static void input() {
-        try (InputStream in = new FileInputStream("data.dat")){
-            int dat = in.read();
-            System.out.println(dat);
+    private static void output() {
+        try (OutputStream out = new FileOutputStream("data.dat")) {
+            out.write(7);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void output() {
-        try(OutputStream out = new FileOutputStream("data.dat")) {
-            out.write(7);
-        }
-        catch (IOException e) {
+    private static void input() {
+        try (InputStream in = new FileInputStream("data.dat")) {
+            int dat = in.read();
+            System.out.println(dat);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
