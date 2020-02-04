e1767f0404345ea091462ac37c9a676dc911bd2b
guswns1659 <guswns1659@gmail.com>
2020-02-04 11:30:21 +0900

    20.02.04) Update .gitignore

---
+++ Code/CSstudying/DoitCh1/Practice15to17.java
@@ -0,0 +1,46 @@
+package CSstudying.DoitCh1;
+
+public class Practice15to17 {
+    public static void main(String[] args) {
+//        triangleLB(5);
+//        triangleLU(5);
+        triangleRU(5);
+        triangleRB(5);
+    }
+
+    private static void triangleRB(int num) {
+    }
+
+    private static void triangleRU(int num) {
+        int starCount = num;
+        int blankCount = num - starCount;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= starCount; j++) {
+                if (blankCount >= j) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println();
+            starCount--;
+        }
+    }
+
+    private static void triangleLU(int num) {
+        int starCount = num;
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= starCount; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            starCount--;
+        }
+    }
+
+    private static void triangleLB(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
