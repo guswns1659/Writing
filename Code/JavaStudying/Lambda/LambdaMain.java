ccbe271b79b3b7b0904721be5e3ee3c09b752032
guswns1659 <guswns1659@gmail.com>
2020-02-09 18:50:44 +0900

    20.02.09) Study Lambda with 열혈 자바

--- Code/JavaStudying/Lambda/LambdaMain.java
+++ Code/JavaStudying/Lambda/LambdaMain.java
@@ -8,18 +8,16 @@ import java.util.stream.Stream;
 
 public class LambdaMain {
     public static void main(String[] args) {
-        String[] names = {
-                "jun", "jack", "poogle", "jason",
-                "hidee", "crong", "honux", "JK"
-        };
+        printTest(s -> System.out.println(s));
+    }
 
-        Stream<String> namesStream = Stream.of(names);
-
-        ArrayList<String> arrayListNames =
-                namesStream.collect(Collectors.toCollection(ArrayList::new));
-        arrayListNames.add("jun");
-        for (String name : arrayListNames) {
-            System.out.println(name);
-        }
+    private static void printTest(Printable printable) {
+        printable.print("string");
     }
 }
+
+interface printable {
+    void print(String string);
+}
+
+
