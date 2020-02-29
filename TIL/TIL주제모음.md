b3165f943aa552cf36c4afca91f8df962a994f1b
guswns1659 <guswns1659@gmail.com>
2020-02-29 11:47:24 +0900

    20.02.29) Feminism 1-4 목차 수정

---
+++ TIL/TIL주제모음.md
@@ -0,0 +1,10 @@
+## Git 
+- 내 local에서 remote branch가 fetch 안될 때, 안 보일때 
+- 원격 브랜치를 지우고 싶을 때! 
+
+## Java 
+- JVM이 class를 static 영역에 로딩하는 시점과 방법은? (JVM의 class Loader 역할)
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Long a = new Long(123);
+        System.out.println(a.compareTo(new Long(1234)));
+    }
+}
