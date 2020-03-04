ac32650f7251a26b6f5c1eb4647c3e366b40c3a3
guswns1659 <guswns1659@gmail.com>
2020-03-04 22:32:55 +0900

    20.03.03) TIL

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1000);
-        stack.push(100);
-        int sum = stack.stream().mapToInt(s -> s).sum();
-        System.out.println(sum);
+        Animal animal = new Animal();
     }
 }
